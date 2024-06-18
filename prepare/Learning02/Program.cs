518dff5c9f56cc341318013034dcf925bb1522d0
JoashPT <joashtantiangco@yahoo.com>
2024-06-18 18:53:22 +0800

    Learning projects for week 02 of cse 210

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Student Teacher";
+        job1._company = "Augusta Evans School";
+        job1._startYear = 2014;
+        job1._endYear = 2014;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Teacher's Assistant";
+        job2._company = "Augusta Evans School";
+        job2._startYear = 2015;
+        job2._endYear = 2017;
+
+        Job job3 = new Job();
+        job3._jobTitle = "History Teacher";
+        job3._company = "Clark-Shaw Magnet School";
+        job3._startYear = 2017;
+        job3._endYear = 2019;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Bruce Vargas";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._jobs.Add(job3);
+
+        resume1.DisplayResume();
     }
 }
