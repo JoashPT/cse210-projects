1b556e5bd2233bb7006f9a633c479325e3e91291
JoashPT <joashtantiangco@yahoo.com>
2024-07-05 16:38:56 +0800

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment01 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine($"{assignment01.GetSummary()}");
+
+        MathAssignment assignment02 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine($"\n{assignment02.GetSummary()}");
+        Console.WriteLine($"{assignment02.GetHomeworkList()}");
+
+        WritingAssignment assignment03 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine($"\n{assignment03.GetSummary()}");
+        Console.WriteLine($"{assignment03.GetWritingInformation()}");
     }
 }
