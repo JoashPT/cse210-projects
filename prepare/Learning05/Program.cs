798e23b0314f20ccc9111fcd7b258b1438bb1b32
JoashPT <joashtantiangco@yahoo.com>
2024-07-10 17:46:28 +0800

    cse 210 learning 05 completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("red", 7, 4));
+        shapes.Add(new Circle("green", 4));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} --- Area: {String.Format("{0:0.##}", shape.GetArea())} units squared");
+        }
     }
 }
